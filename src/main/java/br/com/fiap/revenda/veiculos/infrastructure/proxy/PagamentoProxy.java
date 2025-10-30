c39d30f176573b79c93a2ab184c088f6408881eb
Wesley Junior de Souza <wesjuniorsouza@gmail.com>
2025-10-29 21:23:04 -0300

    inclusao de classe de proxy

--- src/main/java/br/com/fiap/revenda/veiculos/infrastructure/proxy/PagamentoProxy.java
+++ src/main/java/br/com/fiap/revenda/veiculos/infrastructure/proxy/PagamentoProxy.java
@@ -2,8 +2,8 @@ package br.com.fiap.revenda.veiculos.infrastructure.proxy;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "pagamento", url = "http://localhost:8081") // Replace with actual service name and URL
+@FeignClient(name = "pagamento", url = "http://localhost:8081")
 public interface PagamentoProxy {
 
-    
+
 }
