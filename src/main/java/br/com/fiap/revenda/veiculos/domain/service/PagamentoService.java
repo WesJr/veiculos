136c2164afefac196008ffd64bd04b2f5a8f889b
Wesley Junior de Souza <wesjuniorsouza@gmail.com>
2025-10-30 12:34:53 -0300

    integração entre serviço veiculo e pagamento

---
+++ src/main/java/br/com/fiap/revenda/veiculos/domain/service/PagamentoService.java
@@ -0,0 +1,19 @@
+package br.com.fiap.revenda.veiculos.domain.service;
+
+import br.com.fiap.revenda.veiculos.infrastructure.proxy.PagamentoProxy;
+import br.com.fiap.revenda.veiculos.presentation.dto.PagamentoDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PagamentoService {
+
+    private final PagamentoProxy proxy;
+
+    public PagamentoService(PagamentoProxy proxy) {
+        this.proxy = proxy;
+    }
+
+    public void realizarPagamento(PagamentoDto pagamentoDto) {
+        proxy.realizarPagamento(pagamentoDto);
+    }
+}
