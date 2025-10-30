1629b023ca4ad09eec22519dde49b216e325dbee
Wesley Junior de Souza <wesjuniorsouza@gmail.com>
2025-10-29 21:51:22 -0300

    endpoind de pagamento

--- src/main/java/br/com/fiap/revenda/veiculos/infrastructure/proxy/PagamentoProxy.java
+++ src/main/java/br/com/fiap/revenda/veiculos/infrastructure/proxy/PagamentoProxy.java
@@ -1,9 +1,13 @@
 package br.com.fiap.revenda.veiculos.infrastructure.proxy;
 
+import br.com.fiap.revenda.veiculos.presentation.dto.PagamentoDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "pagamento", url = "http://localhost:8081")
 public interface PagamentoProxy {
 
-
+    @PostMapping("/pagamento/realizar")
+    void realizarPagamento(PagamentoDto pagamentoDto);
 }
